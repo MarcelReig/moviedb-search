bb821c78b15d5838361dc8f7e81c6d5185ad7ada
Marcel Reig <marcelreig@gmail.com>
2019-06-02 19:00:20 +0200

    sort by popularity

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const baseURL = "https://api.themoviedb.org/3/";
 const key = "?api_key=0721fb3764192b1547c7f9a484a2391a";
 const key2 = "&api_key=0721fb3764192b1547c7f9a484a2391a";
 const imgUrl = "https://image.tmdb.org/t/p/";
-const imgSize = "w500/";
+const imgSize = "w92/";
 
 $(document).ready(function() {
   // event handlers
@@ -25,16 +25,22 @@ $(document).ready(function() {
     suburl = "discover/movie?sort_by=popularity.desc"; // sort popular movies
     $.ajax({
       url: baseURL + suburl + key2,
-      type: "GET",
-      success: function(movies) {
-        $.each(movies, function(i, movie) {
-          $movies = $("<li></li>");
-          $movies.append(
-            $("<img>").attr("src", imgUrl + imgSize + movie.poster_path)
-          );
+      // url: "books.json",
+      dataType: "json",
+      type: "get",
+      success: function(datos) {
+        console.log(datos);
+        $.each(datos.results, function(i, obj) {
+          $("#popular-movies").append(`
+          <div class="col-md-6 mb-3">
+            <img src="${imgUrl}${imgSize}${obj.poster_path}" class="img-fluid rounded float-left mr-3"><br>
+            <p class="mt-5">${obj.title} ${obj.release_date}</p>
+          </div>
+            `);
         });
       }
     });
   }
+
   makeItems();
 });
