8aba069aeb3ca7f62092cc33f14432fe0e479460
Marcel Reig <marcelreig@gmail.com>
2019-06-05 18:47:36 +0200

    add overview

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   const byPopularity = "discover/movie?sort_by=popularity.desc";
   const key2 = "&api_key=0721fb3764192b1547c7f9a484a2391a";
   const imgUrl = "https://image.tmdb.org/t/p/";
-  const imgSize = "w92/";
+  let imgSize = "w92/";
 
   // event handlers
   $("#titleSearchBtn").on("click", function(e) {
@@ -50,18 +50,50 @@ $(document).ready(function() {
         // console.log(data);
         $.each(data.results, function(i, obj) {
           $("#content").append(`
-            <div class="col-md-6 mb-3">
-              <img src="${imgUrl}${imgSize}${obj.poster_path}" class="img-fluid rounded float-left mr-3"><br>
+            <div class="col-md-4 mb-3">
+              <img src="${imgUrl}${imgSize}${obj.poster_path}" itemId="${obj.id}" class="img-fluid poster rounded float-left mr-3"><br>
               <p class="mt-5">${
                 obj.title
               } <br><small>${obj.release_date}</small></p>
             </div>
             `);
         });
+        $(".poster").on("click", function() {
+          let itemId = $(this).attr("itemId");
+          history.pushState({ page: "detail" }, null, "/movie/" + itemId);
+          //console.log("pushed State in getItemDetail : detail");
+          getItemDetail(itemId);
+        });
       }
     });
   }
   makeItems();
+
+  function getItemDetail(itemId) {
+    url = "https://api.themoviedb.org/3/movie/";
+    $.ajax({
+      url: url + itemId + key,
+      dataType: "jsonp"
+    })
+      .done(function(obj) {
+        let imgSize = "w300/";
+        $("#overview").append(`
+        <div class="col-md-4  py-5 mb-3">
+          <img src="${imgUrl}${imgSize}${obj.poster_path}" itemId=${obj.id} class="img-fluid poster rounded">
+        </div>
+        <div class="col-md-8">
+        <h3 class="mt-5">${
+          obj.title
+        } <br><small>${obj.release_date}</small></h3>
+        <h3>Overview</h3>
+        <p class="lead">${obj.overview}</p>
+        </div>
+        `);
+      })
+      .fail(function(jqxhr, textStatus, error) {
+        console.log("getItemDetail ajax error: " + textStatus + ", " + error);
+      });
+  }
 
   // Search function
   function searchMovie(keyword) {
@@ -89,7 +121,7 @@ $(document).ready(function() {
           } else {
             $("#content").append(`
             <div class="col-md-6 mb-3">
-              <img src="http://placehold.jp/92x138.png" class="img-fluid rounded float-left mr-3"><br>
+              <img src="https://via.placeholder.com/92x138.png" class="img-fluid rounded float-left mr-3"><br>
               <p class="mt-5">${obj.title} <br><small>${
               obj.release_date
             }</small></p>
